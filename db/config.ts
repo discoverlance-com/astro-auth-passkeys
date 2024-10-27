edceacf58f09c25d29ca04bb2c7faabf0e4b1055
Lance Armah-Abraham <lance@vdc.cloud>
2024-10-27 21:09:07 +0000

    feat: setup db tables

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,28 @@
-import { defineDb } from "astro:db";
+import { column, defineDb, defineTable } from "astro:db";
+
+const users = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    email: column.text({ unique: true }),
+  },
+});
+
+const sessions = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    user_id: column.number({
+      references: () => users.columns.id,
+      name: "userId",
+    }),
+    expires_at: column.date({ name: "expires" }),
+  },
+  indexes: [{ on: ["user_id"] }],
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {},
+  tables: {
+    users,
+    sessions,
+  },
 });
