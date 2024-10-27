1e738b27c774651430523a5026c3a1f1db8bb85c
Lance Armah-Abraham <lance@vdc.cloud>
2024-10-27 21:47:12 +0000

    feat: setup astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,15 +9,32 @@ import react from "@astrojs/react";
 
 import db from "@astrojs/db";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-auth-passkeys.discoverlance.com",
+  prefetch: true,
+
   output: "server",
+
+  security: {
+    checkOrigin: true,
+  },
 
   adapter: node({
     mode: "standalone",
   }),
 
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), db()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    db(),
+    sitemap({
+      filter: (page) =>
+        page !== "https://astro-auth-passkeys.discoverlance.com/dashboard/",
+    }),
+  ],
 });
