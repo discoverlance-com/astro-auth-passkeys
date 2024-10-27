31b9686b456d7b99383d595787ada759692139bb
Lance Armah-Abraham <lance@vdc.cloud>
2024-10-27 19:04:28 +0000

    feat: setup node ssr support with shadcn and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+});
